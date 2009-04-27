e35e5a8cc487ec329367b6aaa0403a27267a6f49
Brian Landau <brianjlandau@gmail.com>
2009-04-27 16:14:59 -0400

    Don't use tabs.

--- lib/acts_as_markup/exts/maruku.rb
+++ lib/acts_as_markup/exts/maruku.rb
@@ -5,25 +5,25 @@ class Maruku
   
   attr_reader :text
   
-	def initialize(s=nil, meta={})
-		super(nil)
-		self.attributes.merge! meta
-		if s
-		  @text = s
-			parse_doc(s)
-		end
-	end
-	
-	# Used to get the original Markdown text.
-	def to_s
-	  @text
+  def initialize(s=nil, meta={})
+    super(nil)
+    self.attributes.merge! meta
+    if s
+      @text = s
+      parse_doc(s)
+    end
+  end
+  
+  # Used to get the original Markdown text.
+  def to_s
+    @text
   end
   
   # used to be compatable with Rails/ActiveSupport
   def blank?
     @text.blank?
   end
-	
+  
 end
 
 class String
@@ -44,26 +44,26 @@ module MaRuKu # :nodoc:
       # method because we need +to_html+ to return the original text on plain text fields of
       # the variable language option on +acts_as_markup+
       def array_to_html(array)
-    		elements = []
-    		array.each do |item|
-    			method = item.kind_of?(MDElement) ? "to_html_#{item.node_type}" : "to_xml"
+        elements = []
+        array.each do |item|
+          method = item.kind_of?(MDElement) ? "to_html_#{item.node_type}" : "to_xml"
           unless item.respond_to?(method)
-    				next
-    			end
+            next
+          end
 
-    			html_text =  item.send(method)
+          html_text =  item.send(method)
           if html_text.nil?
-    				raise "Nil html created by method  #{method}:\n#{html_text.inspect}\n for object #{item.inspect[0,300]}"
-    			end
+            raise "Nil html created by method  #{method}:\n#{html_text.inspect}\n for object #{item.inspect[0,300]}"
+          end
 
-    			if html_text.kind_of?Array
-    				elements = elements + html_text
-    			else
-    				elements << html_text
-    			end
-    		end
-    		elements
-    	end
+          if html_text.kind_of?Array
+            elements = elements + html_text
+          else
+            elements << html_text
+          end
+        end
+        elements
+      end
 
     end
   end
