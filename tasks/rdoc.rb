c3d71a41fbd1cdadf6009d624879be90cb649d7e
Brian Landau <brianjlandau@gmail.com>
2010-09-04 21:23:56 -0400

    Update Rdoc task

--- tasks/rdoc.rb
+++ tasks/rdoc.rb
@@ -1,20 +1,12 @@
 begin
-  require 'rake/rdoctask'
   gem 'brianjlandau-sdoc-helpers'
   require 'sdoc_helpers'
+  require 'rake/sdoctask'
   
-  Rake::RDocTask.new do |rdoc|
+  Rake::SDocTask.new do |rdoc|
     version = ActsAsMarkup::VERSION
-
-    rdoc.rdoc_dir = 'docs'
     rdoc.title = "ActsAsMarkup #{version} Documentation"
-    rdoc.rdoc_files.include('README*')
-    rdoc.rdoc_files.include('LICENSE')
-    rdoc.rdoc_files.include('CHANGELOG')
     rdoc.rdoc_files.include('lib/**/*.rb')
-    rdoc.main = 'README.rdoc'
-    rdoc.template = 'direct'
-    rdoc.options << '--fmt' << 'shtml'
   end
 rescue LoadError
   puts "sdoc RDoc tasks not loaded. Please intall sdoc."
